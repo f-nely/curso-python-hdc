2100e17fd7a13c8895cc708e3fdcee43083a5948
nely de faria <nelly.nf54@gmail.com>
2022-02-10 18:35:30 -0300

    Criando arquivo ex005.py

---
+++ curso-python-hdc/section-02/ex005.py
@@ -0,0 +1,15 @@
+a = 5
+b = 3
+c = 10
+d = 2
+
+print(f'{a} > {b} {a > b}')
+print(f'{c} == {a} {c == a}')
+print(f'{d} != {a} {d != a}')
+print(f'{a} <= {c} {a <= c}')
+print(f'{b} > {c} {b > c}')
+print(f'{a} != {c} {a != c}')
+print(f'{d} >= {a} {d >= a}')
+print(f'{d} != {b} {d != b}')
+print(f'{d} < {c} {d < c}')
+print(f'{b} >= {a} {b >= a}')
