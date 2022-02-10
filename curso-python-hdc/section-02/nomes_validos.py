b70288147097a80c795b52a8d3652980a53a6278
nely de faria <nelly.nf54@gmail.com>
2022-02-10 16:42:32 -0300

    Criando arquivo nomes_validos.py

---
+++ curso-python-hdc/section-02/nomes_validos.py
@@ -0,0 +1,24 @@
+import datetime
+
+ano2020 = 2020
+print(ano2020)
+
+# 2020ano = 2020 - nome inválido para variável
+
+_2020ano = 2020
+print(_2020ano)
+
+ano_passado = 2021
+print(ano_passado)
+
+"""currentDateTime = datetime.datetime.now()
+date = currentDateTime.date()
+year = date.strftime('%Y')
+print(f'Current year -> {year}')"""
+
+date = datetime.date.today()
+year = date.strftime('%Y')
+print(year)
+
+full_name = "Matheus Battisti"
+print(full_name)
