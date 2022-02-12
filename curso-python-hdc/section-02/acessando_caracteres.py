8924386ecfc326b985e60a3953ab16ecde125ed8
nely de faria <nelly.nf54@gmail.com>
2022-02-12 14:59:34 -0300

    Criando arquivo acessando_caracteres.py

---
+++ curso-python-hdc/section-02/acessando_caracteres.py
@@ -0,0 +1,18 @@
+word = 'nickname'
+
+print(word)
+print(word[0:4])
+print(word[0])
+
+word_upper = word.upper()
+print(word_upper)
+word_lower = word_upper.lower()
+print(word_lower)
+word_capitalize = word.capitalize()
+print(word_capitalize)
+
+for i in word:
+    print(i)
+
+word_find = word.find('y')
+print(word_find)
