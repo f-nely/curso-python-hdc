8b910a131ec15457dab9e77bf031ac91741bad78
nely de faria <nelly.nf54@gmail.com>
2022-02-12 14:37:00 -0300

    Criando arquivo ex009.py

---
+++ curso-python-hdc/section-02/ex009.py
@@ -0,0 +1,7 @@
+velocidade = int(input('Informe a velocidade: '))
+limite = int(input('Informe o limite máximo: '))
+
+if velocidade >= limite:
+    print('Velocidade maior ou igual limite: ')
+else:
+    print('Velocidade menor que o limite:')
