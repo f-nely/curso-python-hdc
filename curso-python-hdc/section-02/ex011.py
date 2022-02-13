6e7473e687ffe3f9bb6d0bd3b4c9b925d7736abc
nely de faria <nelly.nf54@gmail.com>
2022-02-13 17:49:24 -0300

    Criando arquivo ex011.py

---
+++ curso-python-hdc/section-02/ex011.py
@@ -0,0 +1,6 @@
+name = 'James Gosling'
+age = 66
+print("My name is %s and I'm %d years old." % (name, age))
+
+quantidade_dinheiro = 35.80
+print('Eu tenho um total de R$ %.2f' % quantidade_dinheiro)
