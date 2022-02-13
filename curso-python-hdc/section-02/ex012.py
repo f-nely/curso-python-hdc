6468abcbf3afe6308eaf3b09c7a0a2abc85086eb
nely de faria <nelly.nf54@gmail.com>
2022-02-13 17:49:40 -0300

    Criando arquivo ex012.py

---
+++ curso-python-hdc/section-02/ex012.py
@@ -0,0 +1,7 @@
+poupanca = 42.65
+juros = 0.2
+fatura_cartao = 100
+
+print('Você tem depositado na poupança R$ %.2f e seu rendimento'
+      ' do último mês foi R$ %.1f. E a fatura do seu cartão de '
+      'crédito foi de R$ %d' % (poupanca, juros, fatura_cartao))
