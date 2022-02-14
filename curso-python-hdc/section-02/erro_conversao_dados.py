9d1a70eaacd05fda1dde0ed2b0009580b7064cee
nely de faria <nelly.nf54@gmail.com>
2022-02-14 20:13:10 -0300

    Criando arquivo erro_conversao_dados.py

---
+++ curso-python-hdc/section-02/erro_conversao_dados.py
@@ -0,0 +1,7 @@
+try:
+    num = int(input('Digite um número: '))
+    print(num)
+except:
+    print('Por favor digite um número inteiro.')
+finally:
+    print('Nenhum erro aconteceu. Ou aconteceu, não sei dizer!!')
