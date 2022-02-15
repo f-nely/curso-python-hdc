502535e6fecd9eb2e5961fa1864c326d3c51b31c
nely de faria <nelly.nf54@gmail.com>
2022-02-15 12:50:26 -0300

    Criando arquivo ex026.py

---
+++ curso-python-hdc/section-03/ex026.py
@@ -0,0 +1,10 @@
+categoria = int(input('Digite a categoria - de 1 até 3: '))
+
+if categoria == 1:
+    print('bolsa')
+elif categoria == 2:
+    print('tênis')
+elif categoria == 3:
+    print('mochila')
+else:
+    print('Categoria não foi encontrada.')
