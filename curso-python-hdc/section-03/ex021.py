bb859168d1913b958dac720b434531df589eeda9
nely de faria <nelly.nf54@gmail.com>
2022-02-15 12:45:34 -0300

    Criando arquivo ex021.py

---
+++ curso-python-hdc/section-03/ex021.py
@@ -0,0 +1,6 @@
+rodas = int(input('Digite a quantidade de rodas: '))
+
+if rodas > 2:
+    print('Seu veículo precisa pagar pedágio.')
+if rodas == 2:
+    print('Seu veículo pode passar livremento.')
