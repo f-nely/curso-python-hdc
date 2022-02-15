85f6bdd61d46c34e4d6ef6e10406a892d624e182
nely de faria <nelly.nf54@gmail.com>
2022-02-15 12:45:58 -0300

    Criando arquivo ex022.py

---
+++ curso-python-hdc/section-03/ex022.py
@@ -0,0 +1,9 @@
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
+if num1 > num2:
+    print(f'{num1} é maior que {num2}')
+if num2 > num1:
+    print(f'{num2} é maior que {num1}')
+if num1 == num2:
+    print(f'{num1} é igual a {num2}')
