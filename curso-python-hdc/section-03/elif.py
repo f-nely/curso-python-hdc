5f2fb51aa4a13c3b5e02f76ca13fdbffa1df4236
nely de faria <nelly.nf54@gmail.com>
2022-02-15 12:49:52 -0300

    Criando arquivo elif.py

---
+++ curso-python-hdc/section-03/elif.py
@@ -0,0 +1,8 @@
+nome = input('Digite o seu nome: ')
+
+if nome == 'Matheus':
+    print('Olá, você é um admin!')
+elif nome == "James":
+    print('Olá, você é um produtor de conteúdo!')
+else:
+    print('Olá, você é um usuário comum!')
