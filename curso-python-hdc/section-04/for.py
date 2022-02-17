f72d26426b4e621b0bc40f51135a762102ebfbd8
nely de faria <nelly.nf54@gmail.com>
2022-02-17 12:09:29 -0300

    Criando arquivo for.py

---
+++ curso-python-hdc/section-04/for.py
@@ -0,0 +1,14 @@
+for i in range(0, 10):
+    print(i)
+
+palavra = 'python'
+
+for letra in palavra:
+    print(letra)
+
+nome = 'matheus'
+
+for x in nome:
+    print(x)
+    if x == 'm':
+        print('Primeira letra do nome')
