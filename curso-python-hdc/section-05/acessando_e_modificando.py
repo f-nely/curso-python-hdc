e6ced835a10da78026acc5f32e163198ed450b04
nely de faria <nelly.nf54@gmail.com>
2022-02-20 13:55:59 -0300

    Criando arquivo acessando_e_modificando.py

---
+++ curso-python-hdc/section-05/acessando_e_modificando.py
@@ -0,0 +1,16 @@
+lista = [1, 2, 3, 4, 5]
+
+print(lista[0])
+print(lista[3])
+
+nomes = ['Cezar', 'Steve', 'Diana']
+
+print(nomes)
+nomes[1] = 'Lucas'
+print(nomes)
+
+print('O meu nome é %s' % nomes[1])
+
+nome = nomes[1]
+
+print(nome)
