fe146aa29bb466c624b4a65e4ea9f0d7991506cb
nely de faria <nelly.nf54@gmail.com>
2022-02-25 18:50:49 -0300

    Criando arquivo ex040.py

---
+++ curso-python-hdc/section-05/ex040.py
@@ -0,0 +1,15 @@
+lista = []
+
+# j = 0
+# while j < 5:
+#     l1 = int(input('Digite um número: '))
+#     lista.append(l1)
+#     j += 1
+#
+# print(lista)
+
+for i in range(0, 5):
+    l1 = int(input('Digite um número: '))
+    lista.append(l1)
+
+print(lista)
