607b243243e4a581fee9b37c5805658fb24d70ed
nely de faria <nelly.nf54@gmail.com>
2022-03-10 13:17:06 -0300

    Criando arquivo ex052.py

---
+++ curso-python-hdc/section-06/ex052.py
@@ -0,0 +1,9 @@
+def func_texto(texto):
+    if len(texto) >= 20:
+        return 'Texto longo demais!'
+    else:
+        return 'Texto curto'
+
+
+print(func_texto('curso de angular 13 free com projetos'))
+print(len('curso de angular 13 free com projetos'))
