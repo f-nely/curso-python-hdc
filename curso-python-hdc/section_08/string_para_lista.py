3b0cd167bb862f932b26b690d4a1ea37b3bf144d
nely de faria <nelly.nf54@gmail.com>
2022-03-13 13:28:51 -0300

    Criando arquivo string_para_lista.py

---
+++ curso-python-hdc/section_08/string_para_lista.py
@@ -0,0 +1,8 @@
+course = 'python3'
+print(list(course))  # convertendo string para lista
+
+for i in course:
+    print(i)
+
+lista = ['Curso', 'de', 'Angular', '13']
+print(' '.join(lista))  # convertendo lista para string
