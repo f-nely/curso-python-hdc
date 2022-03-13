26d50a8179ce7a0df9346144c8c371c2f47feb74
nely de faria <nelly.nf54@gmail.com>
2022-03-13 20:05:10 -0300

    Recebendo dados do usuário

--- curso-python-hdc/section_09/main.py
+++ curso-python-hdc/section_09/main.py
@@ -8,6 +8,27 @@ def selecionar_palavra():
     return palavra.upper()
 
 
+# iniciar o jogo
+def jogar(palavra):
+    palavra_a_completar = '-' * len(palavra)
+    advinhou = False
+    letras_utilizadas = []
+    palavras_utilizadas = []
+    tentativas = 6
+
+    # boas vindas ao jogador
+    print('Vamos jogar!')
+    print(exibir_forca(tentativas))
+
+    print(f'Esta é a palavra: {palavra_a_completar}')
+
+    # enquanto o usuário não adivinhar e ainda houver tentativas
+    while not advinhou and tentativas > 0:
+        tentativa = str(input('Digite uma palavra ou letra para continuar: ')).upper()
+
+        print(tentativa)
+
+
 # status do jogo
 def exibir_forca(tentativas):
     estagios = [  # Fim de jogo
@@ -85,8 +106,10 @@ def exibir_forca(tentativas):
     return estagios[tentativas]
 
 
-teste = selecionar_palavra()
-print(teste)
+# iniciação do jogo e continuar jogando
+def iniciar():
+    palavra = selecionar_palavra()
+    jogar(palavra)
 
-teste_estagio = exibir_forca(2)
-print(teste_estagio)
+
+iniciar()
