8254a5f0830d4dd5caa4b7ab76bd53feb96f0c4b
nely de faria <nelly.nf54@gmail.com>
2022-03-13 13:33:54 -0300

    Criando arquivo split.py

---
+++ curso-python-hdc/section_08/split.py
@@ -0,0 +1,11 @@
+itens = 'Placa mãe, memória ram, hd, fonte, leitor_cd_dvd, processador'
+
+itens_computador = itens.split(', ')
+print(itens_computador)
+
+print('-' * 20)
+
+for i in itens_computador:
+    print(i)
+
+print('-' * 20)
